3db8cf1681b60fd94f7ec0dc6cac75da3b5c675b
Kevin Gilpin <kgilpin@gmail.com>
2014-04-28 16:54:42 -0400

    wip event-based method for registering hosts

--- srv/runners/conjur.py
+++ srv/runners/conjur.py
@@ -10,31 +10,22 @@ def register(host_id, minion_id):
 
     host_id *must* be fully qualified
     """
-    _provision_host(host_id)
-    _setup_conjur_ssh(host_id, minion_id)
-
-
+    api = _conjur_api()
+    host = api.create_host(host_id)
+    api.layer(_client_layer_id).add_host(host)
+    salt.modules.event.fire({id: host.id, api_key: host.api_key}, "conjur/host/register")
 
 def deregister(host_id):
     """
     Deregister a host from Conjur by removing it from the clients layer.
     """
     api = _conjur_api()
-    api.layer(_client_layer_id).remove_host(host_id)
-
+    host = api.host(host_id)
+    api.layer(_client_layer_id).remove_host(host)
+    salt.modules.event.fire({id: host.id}, "conjur/host/deregister")
 
 def _conjur_api():
     appliance_url = os.environ['CONJUR_APPLIANCE_URL']
     account = os.environ.get('CONJUR_ACCOUNT', 'conjur')
     conjur.configure(account=account, appliance_url=appliance_url)
     return conjur.new_from_netrc()
-
-
-def _provision_host(host_id):
-    api = _conjur_api()
-    host = api.create_host(host_id)
-    api.layer(_client_layer_id).add_host(host)
-
-
-def _setup_conjur_ssh(host_id, minion_id):
-    pass
