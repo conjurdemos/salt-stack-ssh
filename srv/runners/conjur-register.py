8fe6b85fb0fed2bd1da2e9f558dc86c111881edc
Kevin Gilpin <kgilpin@gmail.com>
2014-04-29 13:59:24 -0400

    rename conjur runner to avoid conflict with conjur api

--- srv/runners/conjur-register.py
+++ srv/runners/conjur-register.py
@@ -1,9 +1,11 @@
 import os
+import re
 import yaml
+import json
 import salt.config
 import conjur
 
-_client_layer_id = "salt-host-factory-1.0.1.dev/clients"
+_client_layer_id = "salt-host-factory-1.0.2.dev/clients"
 
 def register(host_id, minion_id):
     """
@@ -23,12 +25,21 @@ def deregister(host_id):
 
 def _conjur_api():
     conjur_conf = yaml.load(file('/etc/conjur.conf', 'r'))
+    policy = json.load(file('/var/policy.json'))
+    api_keys = policy['api_keys']
+    regex = re.compile('^([^\:]+):host:(.*)')
+    host_identity = [m for k in api_keys.keys() for m in [regex.search(k)] if m][0]
+    host_id = "host/%s" % host_identity.group(2)
+    host_key = api_keys["%s:host:%s" % (host_identity.group(1), host_identity.group(2))]
+
     conjur.configure(account=conjur_conf['account'], appliance_url=conjur_conf['appliance_url'], verify_ssl=False)
-    return conjur.new_from_netrc()
+    return conjur.new_from_key(host_id, host_key)
 
 def _provision_host(host_id):
     api = _conjur_api()
-    host = api.create_host(host_id)
+    host = api.host(host_id)
+    if not host.exists():
+        host = api.create_host(host_id)
     api.layer(_client_layer_id).add_host(host)
 
 def _setup_conjur_ssh(host_id, minion_id):
