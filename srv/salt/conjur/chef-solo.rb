a8d90015a070f8edf1c81fa3f4323bf7376ab5ae
Kevin Gilpin <kgilpin@gmail.com>
2014-04-30 16:29:46 -0400

    correct chef-solo

--- srv/salt/conjur/chef-solo.rb
+++ srv/salt/conjur/chef-solo.rb
@@ -1,4 +1,5 @@
 file_cache_path "/var/chef-solo"
-cookbook_path "/var/chef-solo/cookbooks"
+cookbook_path ["/var/chef/cookbooks"]
+role_path "/var/chef/roles"
 log_level :debug
 json_attribs "/etc/chef/solo.json"
